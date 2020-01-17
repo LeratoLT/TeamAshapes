13f4c9c38479109f3c582eb6cdad8b1225d0a3ae
AbRetroDev <akiros@retrorabbit.co.za>
2020-01-17 11:39:31 +0200

    Lines to calculate the area of a Triangle

--- GitShapesTest/Program.cs
+++ GitShapesTest/Program.cs
@@ -4,9 +4,27 @@ namespace GitShapesTest
 {
     class Program
     {
+        static decimal t_base, t_height, t_length, t_width;
         static void Main(string[] args)
         {
-            Console.WriteLine("This is your Git test shapes project!");
+            Console.WriteLine("Area of Triangle");
+
+            Console.WriteLine("Please enter base value of Triangle");
+            while (!decimal.TryParse(Console.ReadLine(), out t_base))
+            {
+                Console.WriteLine("The value you passed isn't a decimal value");
+                Console.WriteLine("Please Enter a Decimal value");
+            }
+
+                Console.WriteLine("Please enter height of Triangle");
+            while (!decimal.TryParse(Console.ReadLine(), out t_height))
+            {
+                Console.WriteLine("The value you passed isn't a decimal value");
+                Console.WriteLine("Please Enter a Decimal value");
+            }
+
+                Console.WriteLine("The area of a triangle is " + new Triangle().calculate_TriangleArea(t_base, t_height));
+
         }
     }
 }
