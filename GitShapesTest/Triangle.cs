900b5bb487dd5cab6b1ff3ff5aba4bb7e253d6c1
AbRetroDev <akiros@retrorabbit.co.za>
2020-01-17 10:43:15 +0200

    Added a class Triangle with two fields t_height and t_base,
    a fucntion that returns an area of a triangle

---
+++ GitShapesTest/Triangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitShapesTest
+{
+    class Triangle
+    {
+		private decimal t_base, t_height;
+		public decimal getT_base()
+		{
+			return t_base;
+		}
+		public decimal getT_height()
+		{
+			return t_base;
+		}
+		public Triangle(decimal t_base, decimal t_height)
+		{
+			this.t_base = t_base;
+			this.t_height = t_height;
+		}
+		public decimal calculate_area()
+		{
+			return  0.5m * getT_base() * getT_height();
+		}
+	}
+}
