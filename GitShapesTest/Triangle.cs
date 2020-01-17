b6c7362e87b30cf6192abfd95131a58fa3ffd815
AbRetroDev <akiros@retrorabbit.co.za>
2020-01-17 10:58:19 +0200

    Created a class Triangle with one subroutine that calculate_area

--- GitShapesTest/Triangle.cs
+++ GitShapesTest/Triangle.cs
@@ -6,23 +6,9 @@ namespace GitShapesTest
 {
     class Triangle
     {
-		private decimal t_base, t_height;
-		public decimal getT_base()
+	public decimal calculate_TriangleArea(decimal t_base, decimal t_height)
 		{
-			return t_base;
-		}
-		public decimal getT_height()
-		{
-			return t_base;
-		}
-		public Triangle(decimal t_base, decimal t_height)
-		{
-			this.t_base = t_base;
-			this.t_height = t_height;
-		}
-		public decimal calculate_area()
-		{
-			return  0.5m * getT_base() * getT_height();
+			return 0.5m * t_base * t_height;
 		}
 	}
 }
