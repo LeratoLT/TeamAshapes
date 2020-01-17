07cda9cec635bea86e0c0ab3caed5381f6f9a448
Lerato Letsepe <59874274+rabbit-cryptic@users.noreply.github.com>
2020-01-17 11:02:24 +0200

    Added Square class and call from Main()

--- GitShapesTest/Program.cs
+++ GitShapesTest/Program.cs
@@ -7,6 +7,47 @@ namespace GitShapesTest
         static void Main(string[] args)
         {
             Console.WriteLine("This is your Git test shapes project!");
+            Console.Write("Which shape do you want to calculate (Square/Rec/Tri/Cir): ");
+
+            SQStuff();
+            
+            
+            Console.Write("\n\nPress any key to exit..");
+            Console.ReadKey(); 
         }
+        public static void SQStuff()
+        {
+            if (Console.ReadLine().ToUpper().StartsWith('S'))
+            {
+                Console.WriteLine("===============================");
+                Console.Write("Enter length: ");
+                string slength = Console.ReadLine();
+                Console.Write("Enter width: ");
+                string sWidth = Console.ReadLine();
+                decimal length, width;
+                if (decimal.TryParse(slength, out length) && decimal.TryParse(sWidth, out width))
+                {
+                    Squares sq = new Squares();
+                    Console.WriteLine("Area: " + sq.SQArea(length, width).ToString());
+                }
+                else
+                {
+                    Console.Write("Expected format is any positive numeric.");
+                    Console.Write("\n\nTry again (Y/N): ");
+
+                    if (Console.ReadKey().KeyChar == 'Y')
+                    {
+
+                    }
+                    else if (Console.ReadKey().KeyChar == 'N')
+                    {
+                        Console.Write("Thanks bye");
+                    }
+                    else
+                        Console.Write("Thanks bye");
+
+                }
+            }
+            }
     }
 }
