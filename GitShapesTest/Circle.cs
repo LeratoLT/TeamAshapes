347f2c5af07c3d064897cf205687fed867c719f7
JohnOjoRR <59894797+JohnOjoRR@users.noreply.github.com>
2020-01-17 10:34:23 +0200

    Added circle class

---
+++ GitShapesTest/Circle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Circle
+{
+	public Circle()
+	{
+		radius = 0;
+	}
+
+	private decimal radius;
+
+	public decimal calculateCircleArea(int r)
+	{
+		radius = r;
+		decimal pi = Math.PI;
+		decimal area = pi * radius * radius;
+		return area;		
+	}
+}
