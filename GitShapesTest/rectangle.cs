1023750c327c6920fcb4d27d30532136234717f8
Ncedo45 <59874383+Ncedo45@users.noreply.github.com>
2020-01-17 10:55:39 +0200

    changes to rect class

--- GitShapesTest/rectangle.cs
+++ GitShapesTest/rectangle.cs
@@ -2,20 +2,11 @@
 
 public class Class1
 {
-    private double length;
-    private double width;
-
-
-
-    public void Cal_Area()
+    private double area;
+    public double Cal_Area(double length, double width)
     {
-        Console.Write("Please enter the length of your rectangle:");
-        double length = double.Parse(Console.ReadLine());
-        Console.Write("Please enter the width of your rectangle:");
-        double width = double.Parse(Console.ReadLine());
-
-        double area = length * width;
-        Console.WriteLine("The area of your rectangle is", area);
+        area = length * width;
+        return area;
 
     }
 
