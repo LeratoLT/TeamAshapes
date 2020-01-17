8fd56d00bacbbcbc415b93e8ba2efbeccb4e4d75
Ncedo45 <59874383+Ncedo45@users.noreply.github.com>
2020-01-17 10:32:41 +0200

    Adding rect class

---
+++ GitShapesTest/rectangle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Class1
+{
+    private double length;
+    private double width;
+
+
+
+    public void Cal_Area()
+    {
+        Console.Write("Please enter the length of your rectangle:");
+        double length = double.Parse(Console.ReadLine());
+        Console.Write("Please enter the width of your rectangle:");
+        double width = double.Parse(Console.ReadLine());
+
+        double area = length * width;
+        Console.WriteLine("The area of your rectangle is", area);
+
+    }
+
+}
