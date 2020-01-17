6a54af09838bacc61c3f6a59fb6ec92398b99e35
Ncedo45 <59874383+Ncedo45@users.noreply.github.com>
2020-01-17 11:29:16 +0200

    updated the main class

--- GitShapesTest/Program.cs
+++ GitShapesTest/Program.cs
@@ -7,6 +7,13 @@ namespace GitShapesTest
         static void Main(string[] args)
         {
             Console.WriteLine("This is your Git test shapes project!");
+
+            Class1 rec = new Class1();
+            decimal recArea = rec.Cal_Area(55, 4);
+
+
+
+            Console.WriteLine("the area of rectangle", recArea);
         }
     }
 }
