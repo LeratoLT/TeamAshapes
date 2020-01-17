a89e4ebd7b4adac6c49e667870f786913667513c
Lerato Letsepe <59874274+rabbit-cryptic@users.noreply.github.com>
2020-01-17 10:09:09 +0200

    Adding VS project

---
+++ GitShapesTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitShapesTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is your Git test shapes project!");
+        }
+    }
+}
